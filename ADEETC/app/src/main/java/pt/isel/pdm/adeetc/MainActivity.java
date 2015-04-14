9bd2bcb66f5982bb7c3f2fc110e8ccf8d9f14eb4
João H. Trindade <joao.h.trindade@gmail.com>
2015-04-15 00:09:50 +0100

    ADEETC app - Content Provider (incomp.) + MainActivity (test)

--- ADEETC/app/src/main/java/pt/isel/pdm/adeetc/MainActivity.java
+++ ADEETC/app/src/main/java/pt/isel/pdm/adeetc/MainActivity.java
@@ -1,17 +1,51 @@
 package pt.isel.pdm.adeetc;
 
+import android.app.LoaderManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import pt.isel.pdm.adeetc.provider.ADEETCContract;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int LECTURERS_LOADER = 0;
+
+    private ListView listView;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.list);
+
+        adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                null,
+                new String[] {
+                        ADEETCContract.Lecturers.NAME,
+                        ADEETCContract.Lecturers.EMAIL
+                },
+                new int[] {
+                        android.R.id.text1,
+                        android.R.id.text2
+                },
+                0);
+        listView.setAdapter(adapter);
+
+        getLoaderManager().initLoader(LECTURERS_LOADER, null, this);
     }
 
 
@@ -35,5 +69,71 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case LECTURERS_LOADER:
+                return new CursorLoader(
+                        this,
+                        ADEETCContract.Lecturers.CONTENT_URI,
+                        ADEETCContract.Lecturers.PROJECTION_ALL,
+                        null,
+                        null,
+                        null
+                );
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
+
+    public void onTest1(View view) {
+        final ContentResolver resolver = getContentResolver();
+        new Thread() {
+            public void run() {
+                ContentValues values;
+
+                values = createValues(1754, "João Trindade", "jtrindade@cc.isel.ipl.pt");
+                resolver.insert(ADEETCContract.Lecturers.CONTENT_URI, values);
+
+                values = createValues(1234, "Carlos Guedes", "cguedes@cc.isel.ipl.pt");
+                resolver.insert(ADEETCContract.Lecturers.CONTENT_URI, values);
+
+                values = createValues(1369, "Paulo Pereira", "palbp@cc.isel.ipl.pt");
+                resolver.insert(ADEETCContract.Lecturers.CONTENT_URI, values);
+            }
+        }.start();
+    }
+
+    public void onTest2(View view) {
+
+    }
+
+    public void onClear(View view) {
+        final ContentResolver resolver = getContentResolver();
+        new Thread() {
+            public void run() {
+                resolver.delete(ADEETCContract.Lecturers.CONTENT_URI, null, null);
+            }
+        }.start();
+    }
+
+    ContentValues createValues(int code, String name, String email) {
+        ContentValues values = new ContentValues();
+        values.put(ADEETCContract.Lecturers.CODE, code);
+        values.put(ADEETCContract.Lecturers.NAME, name);
+        values.put(ADEETCContract.Lecturers.EMAIL, email);
+        return values;
     }
 }
--- ADEETC/app/src/main/java/pt/isel/pdm/adeetc/provider/ADEETCProvider.java
+++ ADEETC/app/src/main/java/pt/isel/pdm/adeetc/provider/ADEETCProvider.java
@@ -6,6 +6,7 @@ package pt.isel.pdm.adeetc.provider;
  */
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
@@ -43,8 +44,20 @@ public class ADEETCProvider extends ContentProvider {
     }
 
     @Override
+    public String getType(Uri uri) {
+        switch (URI_MATCHER.match(uri)) {
+            case LECTURERS_LST:
+                return ADEETCContract.Lecturers.CONTENT_TYPE;
+            case LECTURERS_OBJ:
+                return ADEETCContract.Lecturers.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+
+    @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
         SQLiteQueryBuilder qbuilder = new SQLiteQueryBuilder();
         switch (URI_MATCHER.match(uri)) {
             case LECTURERS_LST:
@@ -58,35 +71,54 @@ public class ADEETCProvider extends ContentProvider {
                 qbuilder.appendWhere(ADEETCSchema.COL_ID + "=" + uri.getLastPathSegment());
                 break;
         }
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = qbuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
     }
 
     @Override
-    public String getType(Uri uri) {
+    public Uri insert(Uri uri, ContentValues values) {
+        String table;
         switch (URI_MATCHER.match(uri)) {
             case LECTURERS_LST:
-                return ADEETCContract.Lecturers.CONTENT_TYPE;
-            case LECTURERS_OBJ:
-                return ADEETCContract.Lecturers.CONTENT_ITEM_TYPE;
+                table = ADEETCSchema.Lecturers.TBL_NAME;
+                break;
             default:
-                throw new IllegalArgumentException("Unsupported URI: " + uri);
+                throw new IllegalArgumentException();
         }
-    }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        long newId = db.insert(table, null, values);
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, newId);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        String table;
+        switch (URI_MATCHER.match(uri)) {
+            case LECTURERS_LST:
+                table = ADEETCSchema.Lecturers.TBL_NAME;
+                if (selection != null) {
+                    throw new IllegalArgumentException("selection not supported");
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int ndel = db.delete(table, null, null);
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ndel;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        throw new UnsupportedOperationException();
     }
 }

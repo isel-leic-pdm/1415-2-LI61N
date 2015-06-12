c74480781189fdcdbfa2f7d5e2bef0c550949f8e
João H. Trindade <joao.h.trindade@gmail.com>
2015-06-12 21:11:04 +0100

    ParseLogin full example

--- ParseLogin/app/src/main/java/pt/isel/pdm/parselogin/MainActivity.java
+++ ParseLogin/app/src/main/java/pt/isel/pdm/parselogin/MainActivity.java
@@ -2,11 +2,18 @@ package pt.isel.pdm.parselogin;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,8 +57,48 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onSignUp(View view) {
+        final String username = edtUsername.getText().toString().trim();
+        final String password = edtPassword.getText().toString();
+
+        ParseUser newUser = new ParseUser();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        newUser.setEmail(ParseKeys.TEST_EMAIL_ID + username + ParseKeys.TEST_EMAIL_DOM);
+        newUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(MainActivity.this, "SIGNED UP", Toast.LENGTH_SHORT).show();
+                    String token = ParseUser.getCurrentUser().getSessionToken();
+                    txtToken.setText(token);
+                } else {
+                    Log.e("pdm/parselogin", "SIGN UP FAILED", e);
+                    Toast.makeText(MainActivity.this, "SIGN UP FAILED", Toast.LENGTH_SHORT).show();
+                    txtToken.setText("(no token)");
+                    edtUsername.requestFocus();
+                }
+            }
+        });
     }
 
     public void onSignIn(View view) {
+        final String username = edtUsername.getText().toString().trim();
+        final String password = edtPassword.getText().toString();
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Toast.makeText(MainActivity.this, "SIGNED IN", Toast.LENGTH_SHORT).show();
+                    String token = ParseUser.getCurrentUser().getSessionToken();
+                    txtToken.setText(token);
+                } else {
+                    Log.e("pdm/parselogin", "SIGN IN FAILED", e);
+                    Toast.makeText(MainActivity.this, "SIGN IN FAILED", Toast.LENGTH_SHORT).show();
+                    txtToken.setText("(no token)");
+                    edtUsername.requestFocus();
+                }
+            }
+        });
     }
 }

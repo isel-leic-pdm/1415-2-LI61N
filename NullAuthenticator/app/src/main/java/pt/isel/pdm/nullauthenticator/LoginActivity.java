0df86950e4ff2066877b622617081d5bcc311fb5
João H. Trindade <joao.h.trindade@gmail.com>
2015-06-12 22:15:33 +0100

    NullAuthenticator with LoginActivity skeleton

---
+++ NullAuthenticator/app/src/main/java/pt/isel/pdm/nullauthenticator/LoginActivity.java
@@ -0,0 +1,34 @@
+package pt.isel.pdm.nullauthenticator;
+
+import android.accounts.AccountAuthenticatorActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class LoginActivity extends AccountAuthenticatorActivity {
+
+    private EditText edtUsername;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("pdm/nullauth", "sign[up|in] activity created");
+        setContentView(R.layout.activity_login);
+        edtUsername = (EditText)findViewById(R.id.edtUsername);
+        edtPassword = (EditText)findViewById(R.id.edtPassword);
+    }
+
+
+    public void doSignUp(View view) {
+        Log.i("pdm/nullauth", "sign[up|in] activity doSignUp");
+        final String username = edtUsername.getText().toString().trim();
+        final String password = edtPassword.getText().toString();
+
+    }
+}
--- NullAuthenticator/app/src/main/java/pt/isel/pdm/nullauthenticator/NullAuthenticator.java
+++ NullAuthenticator/app/src/main/java/pt/isel/pdm/nullauthenticator/NullAuthenticator.java
@@ -3,16 +3,21 @@ package pt.isel.pdm.nullauthenticator;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 public class NullAuthenticator extends AbstractAccountAuthenticator {
 
+    private final Context mContext;
+
     public NullAuthenticator(Context context) {
         super(context);
         Log.i("pdm/nullauth", "authenticator ctor");
+        mContext = context;
     }
 
     @Override
@@ -24,7 +29,15 @@ public class NullAuthenticator extends AbstractAccountAuthenticator {
     @Override
     public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
         Log.i("pdm/nullauth", "authenticator addAccount");
-        return null;
+
+        Intent intent = new Intent(mContext, LoginActivity.class);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, accountType);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+
+        return bundle;
     }
 
     @Override
